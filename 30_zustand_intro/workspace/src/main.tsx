5d0507980c9c0d9f8927f001142b74bee30664fe
Nils Hartmann <nils@nilshartmann.net>
2024-03-25 10:21:52 +0100

    clean up

--- 30_zustand_intro/workspace/src/main.tsx
+++ 30_zustand_intro/workspace/src/main.tsx
@@ -1,13 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import NotificationApp from "./NotificationApp.tsx";
-
-// Hier gibt es drei Apps:
-// - CounterApp fuer Live-Coding-Beispiel
-// - NotificationApp fuer Uebung
-// - MemoApp fuer Memo-Beispiel
-// - je nach Bedarf 'render' anpassen:
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <NotificationApp />,
