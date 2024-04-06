0b82d5bb64f35debfe9f81b20ea539f8bc63365a
Nils Hartmann <nils@nilshartmann.net>
2024-04-06 17:56:38 +0200

    tests

---
+++ 30_zustand_intro/workspace/src/__test__/NotificationStore.test.tsx
@@ -0,0 +1,12 @@
+import NotificationApp from "../NotificationApp.tsx";
+import { render, screen, renderHook, act } from "@testing-library/react";
+import _userEvent from "@testing-library/user-event";
+import { jest } from "@jest/globals";
+
+test("todo", async () => {
+  // todo:
+  //  - überlege dir was Du testen möchtest, bzw. welche Tests sinnvoll wären
+  //  - Implementiere mindestens einen Test
+  //  - Die Art und Weise, wie du testest, kannst Du selbst entscheiden
+  //     - warum hast Du dich wofür entschieden?
+});
