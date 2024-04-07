6288ff67b7575850e47fcfedc505864e25cf51dd
Nils Hartmann <nils@nilshartmann.net>
2024-04-07 16:00:43 +0200

    tanstack query verzeichnisse

---
+++ 60_tanstack_query/material/45_suspense_preload/src/__test__/mock-data.ts
@@ -0,0 +1,14 @@
+export const mockData = [
+  {
+    id: "P7",
+    date: "2021-01-10T14:03:04.529Z",
+    title: "Keep calm and learn React!",
+    body: "Pommy ipsum air one's dirty linen fork out plum pudding the lakes, naff gallivanting around ridicule knows bugger all about nowt. Jolly full English breakast proper alright geezer chaps at the boozer, football bog roll ended up brown bread. Proper knackered collywobbles Geordie I'm off to Bedfordshire wellies, knee high to a grasshopper i'll be a monkey's uncle a tad curtain twitching grub's up jolly hockey sticks, a cracking slappers and **meat** and two **veg**.\n\nNaff off ridiculous golly gosh Bob's your uncle darling it's spitting, ear hole bowler hat getting on my wick plum pudding supper black pudding, get away with ya completely starkers a comely wench fork out.",
+  },
+  {
+    id: "P9",
+    date: "2020-04-02T10:07:04.529Z",
+    title: "Using Redux with care",
+    body: "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\n\nUt wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.\n\nNam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+  },
+];
